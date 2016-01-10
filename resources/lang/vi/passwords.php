3ff3df1cdf66581d042bb3ad9eaac82a380d7643
viktor <bishido.test@gmail.com>
2016-01-10 12:37:53 +0700

    change languages

--- resources/lang/vi/passwords.php
+++ resources/lang/vi/passwords.php
@@ -1,18 +1,9 @@
-<?php
-return [
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
-];
+<?php 
+	return [
+		'password'	=>	'Mật khẩu tối thiểu 6 ký tự và phải giống nhau.',
+		'reset'	=>	'Mật khẩu đã được cập nhật!',
+		'sent'	=>	'Email reset đã được gửi đến hộp thư của bạn !',
+		'token'	=>	'Password reset token sai.',
+		'user'	=>	'Chúng tôi không thể tìm thấy user với email bạn vừa nhập.',
+	];
+?>
